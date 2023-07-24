707f58b5b6c3624bd30b847bc3a1b8a60d8e17ec
Zobayer Hossain <90368480+Zobayer6@users.noreply.github.com>
2023-07-24 15:37:47 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,33 @@
+const loadCountries = () => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => displayCountries(data));
+}
+
+const displayCountries = countries =>{
+console.log(countries);
+const countriesHTML = countries.map(country => getCountryHTML(country));
+// console.log(countriesHTML[0])
+
+const container = document.getElementById('countries');
+container.innerHTML = countriesHTML.join('');
+
+
+}
+
+const getCountryHTML = country => {
+  return `
+      <div class="country">
+      
+      <h2>Name: ${country.name.common}</h2>
+      <h2>Capital: ${country.capital}</h2>
+      <img src="${country.flags.png}"/>
+      
+      </div>
+  
+  
+  `
+}
+
+
+loadCountries();
